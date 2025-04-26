95f053edf90b31cab46060c1207ee194b7128905
Atique7865 <hasanmohammadatique786@gmail.com>
2025-04-26 17:08:59 +0900

    Update to Calculator App to test CI/CD

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
+from flask import Flask, request
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask Docker + GitHub Actions + EC2!"
+    return '''
+        <h1>Simple Calculator</h1>
+        <form action="/calculate" method="post">
+            Number 1: <input type="text" name="num1"><br><br>
+            Number 2: <input type="text" name="num2"><br><br>
+            Operation (+, -, *, /): <input type="text" name="op"><br><br>
+            <input type="submit" value="Calculate">
+        </form>
+    '''
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    op = request.form['op']
+
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '/':
+        result = num1 / num2 if num2 != 0 else 'Cannot divide by zero'
+    else:
+        result = 'Invalid operation'
+
+    return f'<h2>Result: {result}</h2><br><a href="/">Try Again</a>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
